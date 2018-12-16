0842d2898b8a2fb659f8ef3039dd20cb7b445f07
Johan Denoyer <johan@johandenoyer.fr>
2018-12-16 13:50:04 +0100

    Added debug function

--- dapnet_rubric_send.py
+++ dapnet_rubric_send.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import requests
 import sys
@@ -7,14 +7,23 @@ dapnet_user = "nocall"
 dapnet_pass = "nopass"
 pos = 0
 
+def debug():
+	if ((len(sys.argv)>3) and (str(sys.argv[3]) == "debug")):
+		return True
+	else:
+		return False
+
 if (len(sys.argv)<3):
 	print ("dapnet_rubric_send.py - version 1.0 - by Johan Denoyer (f4wat@f4wat.xyz)")
 	print ("Usage : ")
-	print (sys.argv[0]+" destination_rubric \"message\"")
+	print (sys.argv[0]+" destination_rubric \"message\" [debug]")
+    print ("debug is optionnal (that's why it's between [])")
 	exit(0)
 
 destination = str(sys.argv[1])
 message = str(sys.argv[2])
+if ((len(sys.argv)>3) and (str(sys.argv[3]) == "debug")):
+	debug = True
 headers = {
     'Content-Type': 'application/json',
 }
@@ -22,4 +31,6 @@ headers = {
 data = '{ "rubricName": "'+destination+'", "text": "'+message+'", "number": '+str(pos)+'}'
 
 response = requests.post('http://www.hampager.de:8080/news', headers=headers, data=data, auth=(dapnet_user, dapnet_pass))
+if (debug()):
+    print ("DEBUG : "+str(response.text))
 exit(0)
--- dapnet_send.py
+++ dapnet_send.py
@@ -1,21 +1,30 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import requests
 import sys
 
 dapnet_user = "nocall"
 dapnet_pass = "nopass"
-dapnet_txgroup = "all"
+dapnet_txgroup = "f-all"
 dapnet_emergency = "false"
+
+def debug():
+	if ((len(sys.argv)>3) and (str(sys.argv[3]) == "debug")):
+		return True
+	else:
+		return False
+
 
 if (len(sys.argv)<3):
 	print ("dapnet_send.py - version 1.0 - by Johan Denoyer (f4wat@f4wat.xyz)")
 	print ("Usage : ")
-	print (sys.argv[0]+" destination_call \"message\"")
+	print (sys.argv[0]+" destination_call \"message\" [debug]")
+	print ("debug is optionnal (that's why it's between [])")
 	exit(0)
 
 destination = str(sys.argv[1])
 message = str(sys.argv[2])
+
 headers = {
     'Content-Type': 'application/json',
 }
@@ -23,4 +32,6 @@ headers = {
 data = '{ "text": "'+destination+': '+message+'", "callSignNames": ["'+destination+'"], "transmitterGroupNames": ["'+dapnet_txgroup+'"], "emergency": '+dapnet_emergency+' }'
 
 response = requests.post('http://www.hampager.de:8080/calls', headers=headers, data=data, auth=(dapnet_user, dapnet_pass))
+if (debug()):
+    print ("DEBUG : "+str(response.text))
 exit(0)
